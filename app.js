0a7dfd424689dbb63eb721d22c5370e8df962119
refiqi <refyfadhila96@gmail.com>
2019-01-02 12:18:00 +0700

    Views Template Engine with handlebars

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res)=>{
-    res.send('Initial App');
-});
+// Load Routes
+
+const main = require('./routes/main');
+
+
+// Use Routes
+
+app.use('/', main);
+
+// Use Style and JS
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Set Template Engine
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'home'
+}));
+app.set('view engine', 'handlebars');
+
+
+
+// Server Nodejs
 
 app.listen(port, (err)=>{
     if (err) throw err;
